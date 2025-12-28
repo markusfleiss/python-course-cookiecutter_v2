7d0b729e00b343640801f89c4cc8c714d2f69ad3
markusfleiss <markusfleiss@gmx.at>
2025-12-28 18:39:46 +0100

    Update python-course-cookiecutter_v2

---
+++ tests/fixtures/project_dir.py
@@ -0,0 +1,21 @@
+import json
+import re
+import shutil
+import subprocess
+from copy import deepcopy
+from pathlib import Path
+from typing import Dict
+
+import pytest
+
+from tests.utils.project import generate_project
+
+
+@pytest.fixture(scope="function")
+def project_dir() -> Path:
+    template_values = {
+        "repo_name": "test_repo"
+    }
+    generated_repo_dir: Path = generate_project(template_values=template_values)
+    yield generated_repo_dir
+    shutil.rmtree(generated_repo_dir)
--- tests/test__generate_project.py
+++ tests/test__generate_project.py
@@ -7,16 +7,8 @@ from pathlib import Path
 from typing import Dict
 
 import pytest
-from tests.utils.project import generate_project
 
-@pytest.fixture(scope="function")
-def project_dir() -> Path:
-    template_values = {
-        "repo_name": "test_repo"
-    }
-    generated_repo_dir: Path = generate_project(template_values=template_values)
-    yield generated_repo_dir
-    shutil.rmtree(generated_repo_dir)
+from tests.utils.project import generate_project
 
 def test__can_generate_project(project_dir: Path):
     """
